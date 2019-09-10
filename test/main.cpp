64e23341ad8ab8a6edb94427050c3d1d8799401c
Paul-Louis Ageneau <paul-louis@ageneau.org>
2019-09-10 23:45:31 +0200

    Added STUN server to test

--- test/main.cpp
+++ test/main.cpp
@@ -27,8 +27,11 @@ using namespace rtc;
 using namespace std;
 
 int main(int argc, char **argv) {
-	auto pc1 = std::make_shared<PeerConnection>();
-	auto pc2 = std::make_shared<PeerConnection>();
+	rtc::Configuration config;
+	config.iceServers.emplace_back("stun.l.google.com:19302");
+
+	auto pc1 = std::make_shared<PeerConnection>(config);
+	auto pc2 = std::make_shared<PeerConnection>(config);
 
 	pc1->onLocalDescription([pc2](const Description &sdp) {
 		cout << "Description 1: " << sdp << endl;
