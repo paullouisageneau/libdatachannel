5ddafb31428159f517b6e2f8e71272ac5d438182
Paul-Louis Ageneau <paul-louis@ageneau.org>
2020-05-29 19:57:13 +0200

    Example under GPLv2

--- examples/client/main.cpp
+++ examples/client/main.cpp
@@ -1,22 +1,23 @@
-/**
+/*
+ * libdatachannel client example
  * Copyright (c) 2019-2020 Paul-Louis Ageneau
  * Copyright (c) 2019 Murat Dogan
  * Copyright (c) 2020 Will Munn
+ * Copyright (c) 2020 Nico Chatzi
  * Copyright (c) 2020 Lara Mackey
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "rtc/rtc.hpp"
