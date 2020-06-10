679263c9f7e20d35b91cd01265a1911817c77eee
Paul-Louis Ageneau <paul-louis@ageneau.org>
2020-06-10 18:54:43 +0200

    Change SCTP congestion control to H-TCP

--- src/sctptransport.cpp
+++ src/sctptransport.cpp
@@ -64,8 +64,8 @@ void SctpTransport::Init() {
 
 	usrsctp_sysctl_set_sctp_max_chunks_on_queue(10 * 1024);
 
-	// Change congestion control from the default TCP Reno (RFC 2581) to HighSpeed TCP (RFC 3649)
-	usrsctp_sysctl_set_sctp_default_cc_module(SCTP_CC_HSTCP);
+	// Change congestion control from the default TCP Reno (RFC 2581) to H-TCP
+	usrsctp_sysctl_set_sctp_default_cc_module(SCTP_CC_HTCP);
 
 	// Increase the initial window size to 10 MTUs (RFC 6928)
 	usrsctp_sysctl_set_sctp_initial_cwnd(10);
