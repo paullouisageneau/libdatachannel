d38ce2b57582890f5f9fd8d87f1b96051edd81bb
Filip Klembara <filip@klembara.pro>
2020-11-10 16:02:19 +0100

    Add contributor at the beginning of the edited file

--- src/peerconnection.cpp
+++ src/peerconnection.cpp
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2019 Paul-Louis Ageneau
+ * Copyright (c) 2020 Filip Klembara (in2core)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
