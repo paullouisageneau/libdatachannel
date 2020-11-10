ffa4e4bb20fad0dbd35c0cc45cd375ed906d72a0
Filip Klembara <filip@klembara.pro>
2020-11-10 15:47:56 +0100

    Add reinterpret_pointer_cast implementation to fix build error
    Apple clang does not implement reinterpret_pointer_cast as mentioned here: https://github.com/conda-forge/libcxx-feedstock/issues/44

--- src/peerconnection.cpp
+++ src/peerconnection.cpp
@@ -33,6 +33,17 @@
 #include <iomanip>
 #include <thread>
 
+#if __clang__
+namespace std {
+
+template <typename To, typename From>
+inline std::shared_ptr<To> reinterpret_pointer_cast(std::shared_ptr<From> const & ptr) noexcept {
+    return std::shared_ptr<To>(ptr, reinterpret_cast<To *>(ptr.get()));
+}
+
+}
+#endif
+
 namespace rtc {
 
 using namespace std::placeholders;
