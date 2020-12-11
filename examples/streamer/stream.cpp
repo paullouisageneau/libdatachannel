3f9565b55db90f5f5798429b8e7ce30f480451ae
Filip Klembara <filip@klembara.pro>
2020-12-11 18:44:58 +0100

    Add custom implementation of usleep for windows

--- examples/streamer/stream.cpp
+++ examples/streamer/stream.cpp
@@ -19,6 +19,24 @@
 #include "stream.hpp"
 #include "helpers.hpp"
 
+#if _WIN32
+// taken from https://stackoverflow.com/questions/5801813/c-usleep-is-obsolete-workarounds-for-windows-mingw
+#include <windows.h>
+
+void usleep(__int64 usec)
+{
+    HANDLE timer;
+    LARGE_INTEGER ft;
+
+    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+    timer = CreateWaitableTimer(NULL, TRUE, NULL);
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+    WaitForSingleObject(timer, INFINITE);
+    CloseHandle(timer);
+}
+#endif
+
 void StreamSource::stop() {
     sampleTime_us = 0;
     sample = {};
