895fe88a6287381c95598b265772465fe2844a21
Murat Dogan <doganmurat@gmail.com>
2021-03-12 15:17:33 +0300

    client-benchmark app

---
+++ examples/client-benchmark/main.cpp
@@ -0,0 +1,327 @@
+/*
+ * libdatachannel client-benchmark example
+ * Copyright (c) 2019-2020 Paul-Louis Ageneau
+ * Copyright (c) 2019 Murat Dogan
+ * Copyright (c) 2020 Will Munn
+ * Copyright (c) 2020 Nico Chatzi
+ * Copyright (c) 2020 Lara Mackey
+ * Copyright (c) 2020 Erik Cota-Robles
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "rtc/rtc.hpp"
+
+#include "parse_cl.h"
+
+#include <nlohmann/json.hpp>
+
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <stdexcept>
+#include <thread>
+#include <unordered_map>
+
+using namespace rtc;
+using namespace std;
+using namespace std::chrono_literals;
+
+using json = nlohmann::json;
+
+template <class T> weak_ptr<T> make_weak_ptr(shared_ptr<T> ptr) { return ptr; }
+
+unordered_map<string, shared_ptr<PeerConnection>> peerConnectionMap;
+unordered_map<string, shared_ptr<DataChannel>> dataChannelMap;
+
+string localId;
+
+shared_ptr<PeerConnection> createPeerConnection(const Configuration &config,
+                                                weak_ptr<WebSocket> wws, string id);
+string randomId(size_t length);
+
+// Benchmark
+const size_t messageSize = 65535;
+binary messageData(messageSize);
+atomic<size_t> receivedSize = 0, sentSize = 0;
+
+int main(int argc, char **argv) try {
+	Cmdline params(argc, argv);
+
+	rtc::InitLogger(LogLevel::Info);
+
+	// Benchmark - construct message to send
+	fill(messageData.begin(), messageData.end(), std::byte(0xFF));
+
+	Configuration config;
+	string stunServer = "";
+	if (params.noStun()) {
+		cout << "No STUN server is configured. Only local hosts and public IP addresses supported."
+		     << endl;
+	} else {
+		if (params.stunServer().substr(0, 5).compare("stun:") != 0) {
+			stunServer = "stun:";
+		}
+		stunServer += params.stunServer() + ":" + to_string(params.stunPort());
+		cout << "Stun server is " << stunServer << endl;
+		config.iceServers.emplace_back(stunServer);
+	}
+
+	localId = randomId(4);
+	cout << "The local ID is: " << localId << endl;
+
+	auto ws = make_shared<WebSocket>();
+
+	std::promise<void> wsPromise;
+	auto wsFuture = wsPromise.get_future();
+
+	ws->onOpen([&wsPromise]() {
+		cout << "WebSocket connected, signaling ready" << endl;
+		wsPromise.set_value();
+	});
+
+	ws->onError([&wsPromise](string s) {
+		cout << "WebSocket error" << endl;
+		wsPromise.set_exception(std::make_exception_ptr(std::runtime_error(s)));
+	});
+
+	ws->onClosed([]() { cout << "WebSocket closed" << endl; });
+
+	ws->onMessage([&](variant<binary, string> data) {
+		if (!holds_alternative<string>(data))
+			return;
+
+		json message = json::parse(get<string>(data));
+
+		auto it = message.find("id");
+		if (it == message.end())
+			return;
+		string id = it->get<string>();
+
+		it = message.find("type");
+		if (it == message.end())
+			return;
+		string type = it->get<string>();
+
+		shared_ptr<PeerConnection> pc;
+		if (auto jt = peerConnectionMap.find(id); jt != peerConnectionMap.end()) {
+			pc = jt->second;
+		} else if (type == "offer") {
+			cout << "Answering to " + id << endl;
+			pc = createPeerConnection(config, ws, id);
+		} else {
+			return;
+		}
+
+		if (type == "offer" || type == "answer") {
+			auto sdp = message["description"].get<string>();
+			pc->setRemoteDescription(Description(sdp, type));
+		} else if (type == "candidate") {
+			auto sdp = message["candidate"].get<string>();
+			auto mid = message["mid"].get<string>();
+			pc->addRemoteCandidate(Candidate(sdp, mid));
+		}
+	});
+
+	string wsPrefix = "";
+	if (params.webSocketServer().substr(0, 5).compare("ws://") != 0) {
+		wsPrefix = "ws://";
+	}
+	const string url = wsPrefix + params.webSocketServer() + ":" +
+	                   to_string(params.webSocketPort()) + "/" + localId;
+	cout << "Url is " << url << endl;
+	ws->open(url);
+
+	cout << "Waiting for signaling to be connected..." << endl;
+	wsFuture.get();
+
+	string id;
+	cout << "Enter a remote ID to send an offer:" << endl;
+	cin >> id;
+	cin.ignore();
+	if (id.empty()) {
+		// Nothing to do
+		return 0;
+	}
+	if (id == localId) {
+		cout << "Invalid remote ID (This is my local ID). Exiting...";
+		return 0;
+	}
+
+	cout << "Offering to " + id << endl;
+	auto pc = createPeerConnection(config, ws, id);
+
+	// We are the offerer, so create a data channel to initiate the process
+	const string label = "benchmark";
+	cout << "Creating DataChannel with label \"" << label << "\"" << endl;
+	auto dc = pc->createDataChannel(label);
+
+	dc->onOpen([id, wdc = make_weak_ptr(dc)]() {
+		cout << "DataChannel from " << id << " open" << endl;
+		if (auto dcLocked = wdc.lock()) {
+			cout << "Starting benchmark test. Sending data..." << endl;
+			try {
+				while (dcLocked->bufferedAmount() == 0) {
+					dcLocked->send(messageData);
+					sentSize += messageData.size();
+				}
+			} catch (const std::exception &e) {
+				std::cout << "Send failed: " << e.what() << std::endl;
+			}
+		}
+	});
+
+	dc->onBufferedAmountLow([wdc = make_weak_ptr(dc)]() {
+		auto dcLocked = wdc.lock();
+		if (!dcLocked)
+			return;
+
+		// Continue sending
+		try {
+			while (dcLocked->isOpen() && dcLocked->bufferedAmount() == 0) {
+				dcLocked->send(messageData);
+				sentSize += messageData.size();
+			}
+		} catch (const std::exception &e) {
+			std::cout << "Send failed: " << e.what() << std::endl;
+		}
+	});
+
+	dc->onClosed([id]() { cout << "DataChannel from " << id << " closed" << endl; });
+
+	dc->onMessage([id, wdc = make_weak_ptr(dc)](variant<binary, string> data) {
+		if (holds_alternative<binary>(data))
+			receivedSize += get<binary>(data).size();
+	});
+
+	dataChannelMap.emplace(id, dc);
+
+	const int duration = 20;
+	int printStatCounter = 0;
+	for (int i = 0; i < duration; ++i) {
+		this_thread::sleep_for(1000ms);
+		cout << "#" << i << " Received: " << receivedSize.load() / 1024 << " KB/s"
+		     << "   Sent: " << sentSize.load() / 1024 << " KB/s"
+		     << "   BufferSize: " << dc->bufferedAmount() << endl;
+		receivedSize = sentSize = 0;
+		printStatCounter++;
+
+		if (printStatCounter % 5 == 0) {
+			cout << "Stats# "
+			     << "Received Total: " << pc->bytesReceived() / (1024 * 1024) << " MB"
+			     << "   Sent Total: " << pc->bytesSent() / (1024 * 1024) << " MB"
+			     << "   RTT: " << pc->rtt().value_or(0ms).count() << " ms" << endl;
+			// cout << "Selected Candidates# " << endl;
+			cout << endl;
+		}
+	}
+
+	cout << "Cleaning up..." << endl;
+
+	dataChannelMap.clear();
+	peerConnectionMap.clear();
+	return 0;
+
+} catch (const std::exception &e) {
+	std::cout << "Error: " << e.what() << std::endl;
+	dataChannelMap.clear();
+	peerConnectionMap.clear();
+	return -1;
+}
+
+// Create and setup a PeerConnection
+shared_ptr<PeerConnection> createPeerConnection(const Configuration &config,
+                                                weak_ptr<WebSocket> wws, string id) {
+	auto pc = make_shared<PeerConnection>(config);
+
+	pc->onStateChange([](PeerConnection::State state) { cout << "State: " << state << endl; });
+
+	pc->onGatheringStateChange(
+	    [](PeerConnection::GatheringState state) { cout << "Gathering State: " << state << endl; });
+
+	pc->onLocalDescription([wws, id](Description description) {
+		json message = {
+		    {"id", id}, {"type", description.typeString()}, {"description", string(description)}};
+
+		if (auto ws = wws.lock())
+			ws->send(message.dump());
+	});
+
+	pc->onLocalCandidate([wws, id](Candidate candidate) {
+		json message = {{"id", id},
+		                {"type", "candidate"},
+		                {"candidate", string(candidate)},
+		                {"mid", candidate.mid()}};
+
+		if (auto ws = wws.lock())
+			ws->send(message.dump());
+	});
+
+	pc->onDataChannel([id](shared_ptr<DataChannel> dc) {
+		cout << "DataChannel from " << id << " received with label \"" << dc->label() << "\""
+		     << endl;
+
+		cout << "Starting benchmark test. Sending data..." << endl;
+		cout << "### Check other peer's screen for stats ###" << endl;
+		try {
+			while (dc->bufferedAmount() == 0) {
+				dc->send(messageData);
+				sentSize += messageData.size();
+			}
+		} catch (const std::exception &e) {
+			std::cout << "Send failed: " << e.what() << std::endl;
+		}
+
+		dc->onBufferedAmountLow([wdc = make_weak_ptr(dc)]() {
+			auto dcLocked = wdc.lock();
+			if (!dcLocked)
+				return;
+
+			// Continue sending
+			try {
+				while (dcLocked->isOpen() && dcLocked->bufferedAmount() == 0) {
+					dcLocked->send(messageData);
+					sentSize += messageData.size();
+				}
+			} catch (const std::exception &e) {
+				std::cout << "Send failed: " << e.what() << std::endl;
+			}
+		});
+
+		dc->onClosed([id]() { cout << "DataChannel from " << id << " closed" << endl; });
+
+		dc->onMessage([id, wdc = make_weak_ptr(dc)](variant<binary, string> data) {
+			if (holds_alternative<binary>(data))
+				receivedSize += get<binary>(data).size();
+		});
+
+		dataChannelMap.emplace(id, dc);
+	});
+
+	peerConnectionMap.emplace(id, pc);
+	return pc;
+};
+
+// Helper function to generate a random ID
+string randomId(size_t length) {
+	static const string characters(
+	    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+	string id(length, '0');
+	default_random_engine rng(random_device{}());
+	uniform_int_distribution<int> dist(0, int(characters.size() - 1));
+	generate(id.begin(), id.end(), [&]() { return characters.at(dist(rng)); });
+	return id;
+}
---
+++ examples/client-benchmark/parse_cl.cpp
@@ -0,0 +1,160 @@
+/******************************************************************************
+**
+** parse_cl.cpp
+**
+** Thu Aug  6 19:42:25 2020
+** Linux 5.4.0-42-generic (#46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020) x86_64
+** cerik@Erik-VBox-Ubuntu (Erik Cota-Robles)
+**
+** Copyright (c) 2020 Erik Cota-Robles
+**
+** Definition of command line parser class
+**
+** Automatically created by genparse v0.9.3
+**
+** See http://genparse.sourceforge.net for details and updates
+**
+**
+******************************************************************************/
+
+#include <stdlib.h>
+
+#if defined(_WIN32) || defined(WIN32)
+#include "getopt.h"
+#else
+#include <getopt.h>
+#endif
+
+#include "parse_cl.h"
+
+/*----------------------------------------------------------------------------
+**
+** Cmdline::Cmdline ()
+**
+** Constructor method.
+**
+**--------------------------------------------------------------------------*/
+
+Cmdline::Cmdline (int argc, char *argv[]) // ISO C++17 not allowed: throw (std::string )
+{
+  extern char *optarg;
+  extern int optind;
+  int c;
+
+  static struct option long_options[] =
+  {
+    {"noStun", no_argument, NULL, 'n'},
+    {"stunServer", required_argument, NULL, 's'},
+    {"stunPort", required_argument, NULL, 't'},
+    {"webSocketServer", required_argument, NULL, 'w'},
+    {"webSocketPort", required_argument, NULL, 'x'},
+    {"help", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0}
+  };
+
+  _program_name += argv[0];
+
+  /* default values */
+  _n = false;
+  _s = "stun.l.google.com";
+  _t = 19302;
+  _w = "localhost";
+  _x = 8000;
+  _h = false;
+
+  optind = 0;
+  while ((c = getopt_long (argc, argv, "s:t:w:x:enhv", long_options, &optind)) != - 1)
+    {
+      switch (c)
+        {
+        case 'n':
+          _n = true;
+          break;
+
+        case 's':
+          _s = optarg;
+          break;
+
+        case 't':
+          _t = atoi (optarg);
+          if (_t < 0)
+            {
+              std::string err;
+              err += "parameter range error: t must be >= 0";
+              throw (std::range_error(err));
+            }
+          if (_t > 65535)
+            {
+              std::string err;
+              err += "parameter range error: t must be <= 65535";
+              throw (std::range_error(err));
+            }
+          break;
+
+        case 'w':
+          _w = optarg;
+          break;
+
+        case 'x':
+          _x = atoi (optarg);
+          if (_x < 0)
+            {
+              std::string err;
+              err += "parameter range error: x must be >= 0";
+              throw (std::range_error(err));
+            }
+          if (_x > 65535)
+            {
+              std::string err;
+              err += "parameter range error: x must be <= 65535";
+              throw (std::range_error(err));
+            }
+          break;
+
+        case 'h':
+          _h = true;
+          this->usage (EXIT_SUCCESS);
+          break;
+
+        default:
+          this->usage (EXIT_FAILURE);
+
+        }
+    } /* while */
+
+  _optind = optind;
+}
+
+/*----------------------------------------------------------------------------
+**
+** Cmdline::usage () and version()
+**
+** Print out usage (or version) information, then exit.
+**
+**--------------------------------------------------------------------------*/
+
+void Cmdline::usage (int status)
+{
+  if (status != EXIT_SUCCESS)
+    std::cerr << "Try `" << _program_name << " --help' for more information.\n";
+  else
+    {
+      std::cout << "\
+usage: " << _program_name << " [ -enstwxhv ] \n\
+libdatachannel client implementing WebRTC Data Channels with WebSocket signaling\n\
+   [ -n ] [ --noStun ] (type=FLAG)\n\
+          Do NOT use a stun server (overrides -s and -t).\n\
+   [ -s ] [ --stunServer ] (type=STRING, default=stun.l.google.com)\n\
+          Stun server URL or IP address.\n\
+   [ -t ] [ --stunPort ] (type=INTEGER, range=0...65535, default=19302)\n\
+          Stun server port.\n\
+   [ -w ] [ --webSocketServer ] (type=STRING, default=localhost)\n\
+          Web socket server URL or IP address.\n\
+   [ -x ] [ --webSocketPort ] (type=INTEGER, range=0...65535, default=8000)\n\
+          Web socket server port.\n\
+   [ -h ] [ --help ] (type=FLAG)\n\
+          Display this help and exit.\n";
+    }
+  exit (status);
+}
+
