548be9084643d997684ef5b03cba4a536c85ced5
Paul-Louis Ageneau <paul-louis@ageneau.org>
2021-07-24 10:15:23 +0200

    Changed SITEURL to https://libdatachannel.org

--- pages/publishconf.py
+++ pages/publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://libdatachannel.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
