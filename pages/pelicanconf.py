d5e7637ec7f3a9fe8570eac0ce0a1905b98289de
Paul-Louis Ageneau <paul-louis@ageneau.org>
2021-07-24 10:53:42 +0200

    Added CNAME for GitHub pages

--- pages/pelicanconf.py
+++ pages/pelicanconf.py
@@ -6,6 +6,8 @@ SITENAME = 'libdatachannel'
 SITEURL = ''
 
 PATH = 'content'
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 TIMEZONE = 'Europe/Paris'
 
--- pages/publishconf.py
+++ pages/publishconf.py
@@ -18,7 +18,3 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
