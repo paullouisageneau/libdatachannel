0c71320c68dbbb7641b4be286d4df104a4037f04
Paul-Louis Ageneau <paul-louis@ageneau.org>
2023-04-10 11:34:54 +0200

    Continue polling if WSAPoll() returns WSAENOTSOCK on Windows

--- src/impl/pollservice.cpp
+++ src/impl/pollservice.cpp
@@ -51,9 +51,10 @@ void PollService::join() {
 }
 
 void PollService::add(socket_t sock, Params params) {
-	std::unique_lock lock(mMutex);
+	assert(sock != INVALID_SOCKET);
 	assert(params.callback);
 
+	std::unique_lock lock(mMutex);
 	PLOG_VERBOSE << "Registering socket in poll service, direction=" << params.direction;
 	auto until = params.timeout ? std::make_optional(clock::now() + *params.timeout) : nullopt;
 	assert(mSocks);
@@ -64,8 +65,9 @@ void PollService::add(socket_t sock, Params params) {
 }
 
 void PollService::remove(socket_t sock) {
-	std::unique_lock lock(mMutex);
+	assert(sock != INVALID_SOCKET);
 
+	std::unique_lock lock(mMutex);
 	PLOG_VERBOSE << "Unregistering socket in poll service";
 	assert(mSocks);
 	mSocks->erase(sock);
@@ -178,7 +180,10 @@ void PollService::runLoop() {
 				ret = ::poll(pfds.data(), static_cast<nfds_t>(pfds.size()), timeout);
 
 				PLOG_VERBOSE << "Exiting poll";
-
+#ifdef _WIN32
+				if (ret == WSAENOTSOCK)
+					continue; // prepare again as the fd has been removed
+#endif
 			} while (ret < 0 && (sockerrno == SEINTR || sockerrno == SEAGAIN));
 
 			if (ret < 0)
