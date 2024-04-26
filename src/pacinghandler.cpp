118a692e41d58773ded9426606bec1bd9e9649a0
Paul-Louis Ageneau <paul-louis@ageneau.org>
2024-04-26 22:41:02 +0200

    Fix copyright in pacinghandler.cpp

--- src/pacinghandler.cpp
+++ src/pacinghandler.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 Filip Klembara (in2core)
+ * Copyright (c) 2024 Sean DuBois <sean@siobud.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
