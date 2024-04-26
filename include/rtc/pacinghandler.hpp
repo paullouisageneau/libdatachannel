f63702c669db5f2fd276ef74d8ef1e0b35125d59
Paul-Louis Ageneau <paul-louis@ageneau.org>
2024-04-26 22:40:08 +0200

    Fix copyright in pacinghandler.hpp

--- include/rtc/pacinghandler.hpp
+++ include/rtc/pacinghandler.hpp
@@ -1,6 +1,5 @@
 /**
- * Copyright (c) 2020 Staz Modrzynski
- * Copyright (c) 2020 Paul-Louis Ageneau
+ * Copyright (c) 2024 Sean DuBois <sean@siobud.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
