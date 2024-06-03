d39c70a4146977cf3b0da7b4d723e34a94e9f4b4
Tim Schneider <mail@schneidertim.de>
2024-06-03 22:10:46 +0200

    feat(examples): signaling-server using websockets provided by libdatachannel-capi and jsmn json-parser, written in C

--- examples/signaling-server-libdatachannel-ws-capi/signaling-server.c
+++ examples/signaling-server-libdatachannel-ws-capi/signaling-server.c
@@ -1,6 +1,6 @@
 /**
  * signaling server example for libdatachannel
- * Copyright (c) 2023 Tim Schneider
+ * Copyright (c) 2024 Tim Schneider
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -15,10 +15,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <nlohmann/json.hpp>
+#include "jsmn.h"
 
 #define MAX_PATH_LENGTH 1024
 #define MAX_CLIENTS 1024
+#define MAX_JSON_TOKENS 1024 /* We expect no more than 1024 tokens */
 
 struct ConnectedClients 
 { 
@@ -42,11 +43,11 @@ static void RTC_API my_rtcOpenCallbackFunc(int id, void *ptr){
     
     char path[MAX_PATH_LENGTH];
     memset(path, 0, MAX_PATH_LENGTH);
-    int size = rtcGetWebSocketPath(id, path, MAX_PATH_LENGTH);
+    const int size = rtcGetWebSocketPath(id, path, MAX_PATH_LENGTH);
     path[size] = 0;
     fprintf(stdout, "path:%s\n", path);
 
-    char* user = last_path_segment(path, "/");
+    const char* user = last_path_segment(path, "/");
     fprintf(stdout, "new user login:%s\n", user);
     
     // login user
@@ -62,7 +63,7 @@ static void RTC_API my_rtcOpenCallbackFunc(int id, void *ptr){
         
         if(my_ConnectedClients.ids[i] <= 0 ){
             my_ConnectedClients.ids[i] = id;
-            size_t len = strlen(user);
+            const size_t len = strlen(user);
             my_ConnectedClients.client[i] = (char*) malloc(len+1);
             strncpy(my_ConnectedClients.client[i], user, len);
             ((char*)my_ConnectedClients.client[i])[len]=0; 
@@ -89,41 +90,79 @@ static void RTC_API my_rtcErrorCallbackFunc(int id, const char *error, void *ptr
     rtcClose(id);
 }
 
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+  if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
+      strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+    return 0;
+  }
+  return -1;
+}
+
 static void RTC_API my_rtcMessageCallback(int id, const char *message, int size, void *ptr){
     bool is_binary = size > 0;
-    fprintf(stdout, "message (%s) from websocket #%d (len %d):%s\n", is_binary?"binary":"text", id, size, message);
+    int message_len = abs(size);
+    fprintf(stdout, "message (%s) from websocket #%d (len %d):%s\n", is_binary?"binary":"text", id, message_len, message);
+    
+    int i = 0;
+    int r = 0;
+    jsmn_parser p;
+    jsmntok_t t[MAX_JSON_TOKENS];
+    
+    jsmn_init(&p);
+    r = jsmn_parse(&p, message, message_len, t, sizeof(t) / sizeof(t[0]));
 
-    using nlohmann::json;
-    try
-    {
-        json j = json::parse(message);
-        if(j.contains("id")){
-            std::string dest_id = j["id"];
-            int i_dest = -1;
-            int i_src = -1;
+    /* Assume the top-level element is an object */
+    if (r < 1 || t[0].type != JSMN_OBJECT) {
+        fprintf(stderr, "Object expected: %.*s\n", message_len, message);
+        return;
+    }
+
+    int i_dest = -1;
+    int i_src = -1;
+    const char* dest_id = NULL;
+    int len_dest_id = 0; 
+
+    /* Loop over all keys of the root object */
+    for (i = 1; i < r; i++) {
+        if (jsoneq(message, &t[i], "id") == 0) {
+            /* fetch string value */
+            dest_id = message + t[i + 1].start;
+            len_dest_id = t[i + 1].end - t[i + 1].start; 
             for (int i = 0; i < MAX_CLIENTS; i++)
             {
                 if(my_ConnectedClients.ids[i] < 0) continue;
                 if(my_ConnectedClients.ids[i] == id) i_src = i;
-                if(i_dest < 0 && strcmp(my_ConnectedClients.client[i], dest_id.data()) == 0) i_dest = i;
+                if(i_dest < 0 && strncmp(my_ConnectedClients.client[i], dest_id, len_dest_id) == 0) i_dest = i;
                 if(i_dest >= 0 && i_src >= 0) break;
             }
-            
-            if(i_dest < 0){
-                fprintf(stderr, "No client %s connected.\n", dest_id.data());
-                return;
-            }
-            if(i_src >= 0){
-                j["id"] = std::string(my_ConnectedClients.client[i_src]);
-                auto const new_message = j.dump();
-                int sent = rtcSendMessage(my_ConnectedClients.ids[i_dest], new_message.data(), -(int)new_message.size());
-                return;
-            }
-        };
+
+            i++;
+        }
     }
-    catch (json::parse_error& ex)
-    {
-        fprintf(stderr, "parse error at byte %zd of %s\n", ex.byte, message);
+
+    if(i_dest < 0){
+        fprintf(stderr, "No client %.*s connected.\n", len_dest_id, dest_id);
+        return;
+    }
+    if(i_src >= 0){
+        // in message, replace dest_id with src_id and send data to websocket correspondig to dest_id
+
+        const char* src_id_ptr = my_ConnectedClients.client[i_src];
+        const int src_id_len = (int)strlen(src_id_ptr);
+        
+        const int new_message_len = message_len-len_dest_id+src_id_len;
+        char* new_message = (char*)malloc(new_message_len);
+        const int o_id = (int)(dest_id-message); // offset id value
+        strncpy(new_message, message, o_id); // characters before id
+        strncpy(new_message+o_id, src_id_ptr, src_id_len); // insert src_id
+        strncpy(new_message+o_id+src_id_len, dest_id+len_dest_id, message_len-o_id-len_dest_id); // characters after id
+
+        fprintf(stdout, "message (%s) to websocket #%d (len %d): %.*s", 
+            is_binary?"binary":"text", my_ConnectedClients.ids[i_dest], 
+            (int)new_message_len, (int)new_message_len, new_message);
+        int sent = rtcSendMessage(my_ConnectedClients.ids[i_dest], 
+            new_message, (int) is_binary?new_message_len:-(new_message_len)); // negative message length indicates text-message instead of binary messsage 
+        free(new_message);
     }
     // if either no json or no matching client found
 }
