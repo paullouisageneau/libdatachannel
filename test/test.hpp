fd260c860eb623bfedb728f0b49796bd4d9c6f06
Michał Śledź <michalsledz34@gmail.com>
2025-07-15 09:56:23 +0200

    Add copyright notes

--- test/test.hpp
+++ test/test.hpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2025 Michal Sledz
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include <functional>
 #include <iostream>
 
