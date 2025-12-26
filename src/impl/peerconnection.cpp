71be547ee3099f42ec70f12d8bfa49465b689c07
Alexander Reznichenko <avreznichenko_2@edu.hse.ru>
2025-12-26 16:23:25 +0300

    Fix

--- src/impl/peerconnection.cpp
+++ src/impl/peerconnection.cpp
@@ -584,7 +584,7 @@ void PeerConnection::dispatchMedia([[maybe_unused]] message_ptr message) {
 			} else if (header->payloadType() == 206) {
 				auto rtcpfb = reinterpret_cast<RtcpFbHeader *>(header);
 				ssrcs.insert(rtcpfb->packetSenderSSRC());
-				if (header->reportCount() == 15 && header->lengthInBytes() == sizeof(RtcpRemb)) {
+				if (header->reportCount() == 15 && header->lengthInBytes() >= sizeof(RtcpRemb)) {
 					auto remb = reinterpret_cast<RtcpRemb *>(header);
 					if (remb->_id[0] == 'R' && remb->_id[1] == 'E' && remb->_id[2] == 'M' && remb->_id[3] == 'B') {
                     	unsigned numSsrc = remb->getNumSSRC();
