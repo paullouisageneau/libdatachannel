a02b751917ac8afc8c58dc6f4461d25ff9465d48
Paul-Louis Ageneau <paul-louis@ageneau.org>
2026-01-18 22:56:31 +0100

    Bumped version to 0.24.1

--- include/rtc/version.h
+++ include/rtc/version.h
@@ -3,7 +3,7 @@
 
 #define RTC_VERSION_MAJOR 0
 #define RTC_VERSION_MINOR 24
-#define RTC_VERSION_PATCH 0
-#define RTC_VERSION "0.24.0"
+#define RTC_VERSION_PATCH 1
+#define RTC_VERSION "0.24.1"
 
 #endif
