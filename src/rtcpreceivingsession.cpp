f2632b89c8cd6e85c824a75c65b91d0e31198c11
Paul-Louis Ageneau <paul-louis@ageneau.org>
2026-03-28 21:40:58 +0100

    Fix RTCP RR/SR size checks in RtcpReceivingSession

--- src/rtcpreceivingsession.cpp
+++ src/rtcpreceivingsession.cpp
@@ -27,6 +27,7 @@
 namespace rtc {
 
 static impl::LogCounter COUNTER_BAD_RTP_HEADER(plog::warning, "Number of malformed RTP headers");
+static impl::LogCounter COUNTER_BAD_RTCP_HEADER(plog::warning, "Number of malformed RTCP headers");
 static impl::LogCounter COUNTER_UNKNOWN_PPID(plog::warning, "Number of Unknown PPID messages");
 static impl::LogCounter COUNTER_BAD_NOTIF_LEN(plog::warning,
                                               "Number of Bad-Lengthed notifications");
@@ -73,13 +74,29 @@ void RtcpReceivingSession::incoming(message_vector &messages, const message_call
 		}
 
 		case Message::Control: {
-			auto rr = reinterpret_cast<const RtcpRr *>(message->data());
-			if (rr->header.payloadType() == 201) { // RR
+			if (message->size() < sizeof(RtcpHeader)) {
+				COUNTER_BAD_RTCP_HEADER++;
+				PLOG_VERBOSE << "RTCP packet is too small, size=" << message->size();
+				continue;
+			}
+			auto header = reinterpret_cast<const RtcpHeader *>(message->data());
+			if (header->payloadType() == 201) { // RR
+				if (message->size() < RtcpRr::SizeWithReportBlocks(0)) {
+					COUNTER_BAD_RTCP_HEADER++;
+					PLOG_VERBOSE << "RTCP RR is too small, size=" << message->size();
+					continue;
+				}
+				auto rr = reinterpret_cast<const RtcpRr *>(message->data());
 				mSsrc = rr->senderSSRC();
 				rr->log();
-			} else if (rr->header.payloadType() == 200) { // SR
-				mSsrc = rr->senderSSRC();
+			} else if (header->payloadType() == 200) { // SR
+				if (message->size() < RtcpSr::Size(0)) {
+					COUNTER_BAD_RTCP_HEADER++;
+					PLOG_VERBOSE << "RTCP SR is too small, size=" << message->size();
+					continue;
+				}
 				auto sr = reinterpret_cast<const RtcpSr *>(message->data());
+				mSsrc = sr->senderSSRC();
 				{
 					std::lock_guard lock(mSyncMutex);
 					mSyncTimestamps.rtpTimestamp = sr->rtpTimestamp();
