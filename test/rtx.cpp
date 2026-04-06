6e6383e69308f3b27b73ebee75e53b631abcdbf7
Aron Rosenberg <aron.rosenberg@apple.com>
2026-04-06 16:49:05 -0700

    Update copyright name

--- test/rtx.cpp
+++ test/rtx.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2026 Paul-Louis Ageneau
+ * Copyright (c) 2026 Apple Inc.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
